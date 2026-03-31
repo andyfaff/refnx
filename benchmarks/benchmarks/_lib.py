d1f8304933a5559b0f4311cfa1f1bdf870109aa4
Andrew Nelson <andyfaff@gmail.com>
2026-03-31 11:55:48 +1100

    BENCH: flatten, unique

--- benchmarks/benchmarks/_lib.py
+++ benchmarks/benchmarks/_lib.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 from refnx.analysis import Parameter, Parameters
-from refnx._lib._cutil import c_flatten
-from refnx._lib.util import flatten
+from refnx._lib._cutil import c_flatten, c_unique
+from refnx._lib.util import flatten, unique
 
 from .common import Benchmark
 
@@ -19,14 +19,21 @@ def create(items, params, rng):
     return params
 
 
-class Flatten(Benchmark):
+class Util(Benchmark):
     def setup(self):
         rng = np.random.default_rng(1892908)
         p = Parameters()
         self.p = create(20, p, rng)
+        self.flattened = flatten(self.p)
 
-    def time_cflatten(self):
+    def time_c_flatten(self):
         list(c_flatten(self.p))
 
     def time_flatten(self):
         list(flatten(self.p))
+
+    def time_unique(self):
+        list(unique(self.flattened))
+
+    def time_c_unique(self):
+        list(c_unique(self.flattened))
